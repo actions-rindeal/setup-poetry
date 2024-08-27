2ac2845799af00bdf672dfed203c4d7b7d80cb25
rindeal <dev.rindeal@gmail.com>
2024-08-27 23:50:59 +0000

    up

--- vars.js
+++ vars.js
@@ -2,50 +2,43 @@ const fs = require('fs')
 const { execFileSync } = require('child_process')
 
 /**
- * @typedef {import('@actions/github/lib/context').Context} Context
- * @typedef {import('@actions/core')} core
- * @typedef {import('@actions/exec')} exec
- * @typedef {import('@actions/github/lib/utils').GitHub} GitHub
- * @typedef {import('@actions/glob')} glob
- * @typedef {import('@actions/io')} io
- */
-
-/**
- * @param {Context} context - The GitHub Actions context
- * @param {core} core - The core functions from GitHub Actions
- * @param {InstanceType<GitHub>} github - The GitHub API client
- * @param {exec} exec - The exec functions from GitHub Actions
- * @param {glob} glob - The glob functions from GitHub Actions
- * @param {io} io - The io functions from GitHub Actions
- * @param {NodeRequire} require - The Node.js require function
+ * @param {import('@actions/github/lib/context').Context} context
+ * @param {import('@actions/core')} core
+ * @param {InstanceType<import('@actions/github/lib/utils').GitHub>} github
+ * @param {import('@actions/exec')} exec
+ * @param {import('@actions/glob')} glob
+ * @param {import('@actions/io')} io
+ * @param {NodeRequire} require
  */
 async function main(context, core, github, exec, glob, io, require) {
     /**
-     * Executes a command with arguments and returns the trimmed output.
-     * @param {string} command - The command to execute.
-     * @param {string[]} args - The arguments for the command.
-     * @returns {string} - The trimmed output of the command.
+     * Executes a Python command to retrieve user site-packages directory.
+     * @param {string} dirType - The user directory type (e.g., 'base', 'site').
+     * @returns {Promise<string>} - The result of the Python command execution.
      */
-    const execWithAnnounce = (command, args) => {
-        core.debug(`Executing: ${[command, ...args].map(arg => `\`${arg}\``).join(' ')}`)
-        return execFileSync(command, args, { encoding: 'utf8' }).trim()
+    const execPySite = async (dirType) => {
+        const [cmd, args] = ['python3', ['-m', 'site', `--user-${dirType.toLowerCase()}`]]
+        core.debug(`Executing: ${[cmd, ...args].map(arg => `\`${arg}\``).join(' ')}`)
+        const result = execFileSync(cmd, args, { encoding: 'utf8' }).trim()
+        core.debug(`Python site.USER_${dirType.toUpperCase()} = \`${result}\``)
+        return result
     }
 
     /**
-     * Gets the Python site key by executing site commands.
-     * @returns {Promise<string>} - The Python site key.
+     * Gets the Python site cache key by executing site commands.
+     * @returns {Promise<string>} - The Python site cache key.
      */
-    const getPythonSiteKey = async () => {
-        const [ base, site ] = await Promise.all([
-            execWithAnnounce('python3', ['-m', 'site', '--user-base']),
-            execWithAnnounce('python3', ['-m', 'site', '--user-site']),
+    const getPythonSiteCacheKey = async () => {   
+        const [base, site] = await Promise.all([
+            execPySite('base'),
+            execPySite('site'),
         ])
-        core.debug(`Python site.USER_BASE = \`${base}\``)
-        core.debug(`Python site.USER_SITE = \`${site}\``)
+
         const key = site.slice(base.length)
-        core.debug(`Python site key = \`${key}\``)
+        core.debug(`Python site cache key = \`${key}\``)
+
         return key
-    }
+      }
 
     /**
      * Gets the specified or latest Poetry version.
@@ -55,6 +48,7 @@ async function main(context, core, github, exec, glob, io, require) {
     const getPoetryVersion = async () => {
         const inputs = JSON.parse(process.env['INPUTS'])
         const version = inputs['poetry-version']
+
         if ( typeof version === 'undefined' ) {
             throw new Error(`'poetry-version' input is undefined`)
         }
@@ -80,21 +74,44 @@ async function main(context, core, github, exec, glob, io, require) {
      */
     const getVenvBaseDir = async () => {
         const dir = process.env['VENV_BASE_DIR']
-        if ( ! fs.existsSync(dir) || ! fs.statSync(dir).isDirectory() ) {
-            throw new Error(`VENV_BASE_DIR \`${dir}\` is not a valid directory`)
+        if ( ! dir ) {
+            throw new Error('VENV_BASE_DIR is not set')
         }
+    
+        const statPromise = () => new Promise((resolve, reject) => {
+            fs.stat(dir)
+                .then(stats => {
+                    if ( ! stats.isDirectory() ) {
+                        reject(new Error(`VENV_BASE_DIR \`${dir}\` is not a valid directory`))
+                    } else {
+                        resolve()
+                    }
+                })
+                .catch(reject)
+        })
+    
         try {
-            fs.accessSync(dir, fs.constants.W_OK)
+            await Promise.all([
+                statPromise(),
+                fs.access(dir, fs.constants.W_OK)
+            ])
         } catch (err) {
-            throw new Error(`VENV_BASE_DIR \`${dir}\` is not writable`)
+            if ( err.code === 'ENOENT' ) {
+                throw new Error(`VENV_BASE_DIR \`${dir}\` does not exist`)
+            } else if ( err.code === 'EACCES' ) {
+                throw new Error(`VENV_BASE_DIR \`${dir}\` is not writable`)
+            } else {
+                throw err
+            }
         }
+    
         return dir
-    }
+    }    
 
     const outputs = {
         'poetry-version':   await getPoetryVersion(),
         'venv-base-dir': `${await getVenvBaseDir()}/.poetry-venv`,
-        'python-site-key':  await getPythonSiteKey(),
+        'python-site-key':  await getPythonSiteCacheKey(),
     }
     for (const [key, value] of Object.entries(outputs)) {
         core.debug(`Setting output: \`${key}\` = \`${value}\``)
